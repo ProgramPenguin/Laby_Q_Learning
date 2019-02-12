ce4c356c67184d46f073cfa9ae170456e507488d
Ander Jimenez Garcia <ander.jim64@gmail.com>
2019-02-12 15:09:53 +0100

    Changed load_labyrinth and added moves fucntion

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -1,5 +1,14 @@
 import numpy as np;
 
+
+# slot types
+# 0 - nothing
+# 1 - entry
+# 2 - exit
+# 3 - wall
+# 4 - trap
+
+
 class Labyrinthe:
 
     def __init__(self, mat_laby):
@@ -13,15 +22,50 @@ class Labyrinthe:
         file_lines = file.readlines()
 
         # read the labyrinth's dimensions and initializing
-        nb_line = file_lines[0][0]
-        nb_col = file_lines[0][2]
-        self.laby = np.zeros((int(nb_line),int(nb_col)))
+        nb_line = int(file_lines[0][0])+1
+        nb_col = int(file_lines[0][2])+1
+        self.laby = np.zeros((nb_line,nb_col))
 
         for line in file_lines[1:]:
             car_line = line.split(" ")
-            self.laby[int(car_line[0])][int(car_line[1])]=int(car_line[2])
+            self.laby[int(car_line[0])+1][int(car_line[1])+1]=int(car_line[2])
+
+        #fill borders with walls
+        for i in range(nb_col):
+            self.laby[0][i]=3
+            self.laby[nb_line-1][i] = 3
+        for i in range(1,nb_line-1):
+            self.laby[i][0] = 3
+            self.laby[i][nb_col-1] = 3
+
+
+    def get_moves(self,pos_x,pos_y):
+
+        moves = []
+
+        # if we are on a wall
+        if(self.laby[pos_x][pos_y] == 3):
+            return moves
+        else :
+            # check left
+            if(self.laby[pos_x-1][pos_y] != 3):
+                moves.extend(["left"])
+            # checks right
+            if (self.laby[pos_x+1][pos_y] != 3):
+                moves.extend(["right"])
+            # checks up
+            if (self.laby[pos_x][pos_y+1] != 3):
+                moves.extend(["down"])
+            # checks down
+            if (self.laby[pos_x][pos_y-1] != 3):
+                moves.extend(["up"])
+        return moves
+
+
+
 
 
 test = Labyrinthe([])
 test.load_labyrinthe("data/test.txt")
 print(test.laby)
+print(test.get_moves(1,2))
