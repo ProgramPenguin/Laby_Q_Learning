2ee0e28a829700acad9dd94602c950b82eda10f4
Ander Jimenez Garcia <ander.jim64@gmail.com>
2019-03-13 14:33:36 +0100

    Added exploration and epsilon-greedy, reset robot when exit reached

--- QLearning.py
+++ QLearning.py
@@ -1,5 +1,5 @@
 import Labyrinthe as lb
-from random import randint
+import random
 import numpy as np
 
 def get_entries(lab):
@@ -14,7 +14,7 @@ def get_entries(lab):
 
     return list_entries
 
-def exploration(nb_max_move,gamma,lab,rewards):
+def exploration(nb_max_move,gamma,lab,rewards,epsilon):
 
     lab.rewards = rewards
     entries = get_entries(lab)
@@ -25,15 +25,32 @@ def exploration(nb_max_move,gamma,lab,rewards):
 
     for nb_move in range(nb_max_move):
 
+        # On obtient les mouvements possibles
         moves = lab.get_moves(pos[0],pos[1])
-        Q_moves = Q_tab[pos[0]][pos[1]]
-        Q_moves_max = np.where(Q_moves == max(Q_moves[moves]))
-        selected_index = Q_moves_max[0][randint(0,len(Q_moves_max[0])-1)]
+
+        # On choisit au hasard de façon pondere si on explore ou on exploite
+        choix_exp = random.random() # va de 0 a 1
+        if(0.0 < choix_exp < epsilon):
+            #Exploration
+            selected_index = random.randint(0,len(moves)-1)
+            print("exploration")
+        else:
+            # Exploitation
+            #On recupere les valeurs de Q pour la case
+            Q_moves = Q_tab[pos[0]][pos[1]]
+            #On recupere les valeurs max de Q pour la case
+            Q_moves_max = np.where(Q_moves == max(Q_moves[moves]))
+            #On choisit une action au hasard parmis celles avec Q maximal
+            selected_index = Q_moves_max[0][random.randint(0,len(Q_moves_max[0])-1)]
+            print("exploitation")
 
         r, new_pos = lab.move(pos[0],pos[1],selected_index)
         Q_tab[pos[0]][pos[1]][selected_index] = r + gamma * max(Q_tab[new_pos[0]][new_pos[1]])
         pos = new_pos
+        if(lab.laby[pos[0]][pos[1]] == 2):
+            pos = entries[0]
         nb_move = nb_move +1
+    # print(Q_tab)
     return Q_tab
 
 
@@ -42,4 +59,4 @@ def exploration(nb_max_move,gamma,lab,rewards):
 test = lb.Labyrinthe([],[])
 test.load_labyrinthe("data/test.txt")
 print(test.laby)
-exploration(5000,0.5,test,[-1,-25,100,-50])
+exploration(5000,0.5,test,[-1,-25,100,-50],0.5)
